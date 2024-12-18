c80eed0034ccfbbeee760f71bc8798e8f05d250d
Adolpho Piazza <adolphopiazza@gmail.com>
2024-12-18 17:30:44 -0300

    Initial commit

---
+++ Nooro-Weather-App/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Nooro-Weather-App
+//
+//  Adolpho Francisco Zimmermann Piazza
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nooro-Weather-App/Nooro_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nooro_Weather_AppApp.swift
+//  Nooro-Weather-App
+//
+//  Adolpho Francisco Zimmermann Piazza
+//
+
+import SwiftUI
+
+@main
+struct Nooro_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
